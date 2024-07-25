52c1c05836327dceeb842ec095bf4ee5c23c8f3b
bhakti <bhaktishinde2388@gmail.com>
2024-07-25 15:06:15 +0530

    found data used by id and used findOne for finding the single plant data

--- controllers/plant.js
+++ controllers/plant.js
@@ -67,11 +67,12 @@ const getPlants = async (req,res)=>
         })
     }
 
-const getPlantId = (req,res)=>{
+const getPlantId = async (req,res)=>{
     const {id} = req.params
 
-    const plant = plants.find((p)=>p.id == id)
-
+    const plant = await Plant.findOne({
+        _id:id
+    })
     res.json({
         success:plant ? true : false,
         data:plant,
