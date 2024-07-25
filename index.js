1b724af8eacdf46376268074a0cca515da61b5e3
bhakti <bhaktishinde2388@gmail.com>
2024-07-25 13:23:40 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import dotenv from "dotenv"
 dotenv.config()
+import mongoose from "mongoose";
 
 import {getHealth} from "./controllers/health.js"
 import { postPlant } from "./controllers/plant.js";
@@ -8,39 +9,25 @@ import { getPlants , getPlantId ,putPlantId ,deletePlantId} from "./controllers/
 import { pageNotFound } from "./controllers/error.js";
 
 
+
 const app = express()
 app.use(express.json())
 
-//temp. db / resourse
-const plants = [
-    {
-        "id": 1,
-        "name": "bambu",
-        "img": "https://m.media-amazon.com/images/I/61hC8AAD1AL._SL1000_.jpg",
-        "price": 40,
-        "category": "indoor",
-        "description": "Ugaoo Lucky Bamboo 3 Layer Feng Shui Plant"
-    },
-    {
-        "id": 3,
-        "name": "rose",
-        "img": "https://m.media-amazon.com/images/I/61hC8AAD1AL._SL1000_.jpg",
-        "price": 40,
-        "category": "indoor",
-        "description": "Ugaoo Lucky Bamboo 3 Layer Feng Shui Plant"
-    },
-    {
-        "id":7,
-        "name": "mango",
-        "img": "https://m.media-amazon.com/images/I/61hC8AAD1AL._SL1000_.jpg",
-        "price": 40,
-        "category": "indoor",
-        "description": "Ugaoo Lucky Bamboo 3 Layer Feng Shui Plant"
-    }
-]
+const dbConnection = async ()=>{
+    const conn = await mongoose.connect("mongodb+srv://bhaktishinde2388:2auLQGdFm4cPC2tM@cluster0.u9doj1t.mongodb.net/nursery-server")
 
+    if(conn){
+        console.log("mongodb connected")
+    }
+    else{
+        console.log("mongodb not connected")
+    }
+}
+dbConnection();
 
 app.get("/health",getHealth)
+
+
 app.post("/plant",postPlant)
 // read resourse
 app.get("/plants",getPlants)
@@ -56,7 +43,7 @@ app.delete("/plant/:id",deletePlantId)
 
 app.use("*", pageNotFound)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT,()=>{
     console.log(`servery is run on ${PORT}`)
